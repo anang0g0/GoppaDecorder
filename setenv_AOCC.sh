0235d56a4faeead82f70e0d56325a409645deb7a
test <test@chagai>
2020-07-31 02:03:08 +0900

    setenv_AOCC.sh

--- setenv_AOCC.sh
+++
@@ -1,8 +0,0 @@
- 
-export PATH=/home/test/ダウンロード/aocc-compiler-2.1.0/bin:$PATH
-export LIBRARY_PATH=/home/test/ダウンロード/aocc-compiler-2.1.0/lib:/home/test/ダウンロード/aocc-compiler-2.1.0/lib32:$LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/test/ダウンロード/aocc-compiler-2.1.0/lib:/home/test/ダウンロード/aocc-compiler-2.1.0/lib32:$LD_LIBRARY_PATH
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:/home/test/ダウンロード/aocc-compiler-2.1.0/include
-export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/home/test/ダウンロード/aocc-compiler-2.1.0/include
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/lib32:$LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/lib:/usr/lib32:$LD_LIBRARY_PATH
