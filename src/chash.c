c7ba651a6a18e9847f038282cc63fc17a67e852c
Osamu Sakai <fumumue@protonmail.com>
2021-05-06 14:39:21 +0900

    bugfix

--- src/chash.c
+++ src/chash.c
@@ -156,7 +156,7 @@ void rp2(unsigned char *a)
   int i, j, x;
   time_t t;
 
-  srand(clock() + time(&t));
+//  srand(clock() + time(&t));
 
   for (i = 0; i < NN; i++)
   {
@@ -185,17 +185,24 @@ arrayul
 chash()
 {
   int i; //, j = 0;
-  arrayn n = {0};
+  arrayul n = {0};
   arrayul vw = {0};
-  unsigned char key[NN] = {148, 246, 52, 251, 16, 194, 72, 150, 249, 23, 90, 107, 151, 42, 154, 124}; //, 48, 58, 30, 24, 42, 33, 38, 10, 115, 41, 164, 16, 33, 32, 252, 143, 86, 175, 8, 132, 103, 231, 95, 190, 61, 29, 215, 75, 251, 248, 72, 48, 224, 200, 147, 93, 112, 25, 227, 223, 206, 137, 51, 88, 109, 214, 17, 172};
+  unsigned char key[NN] = {0}; //{148, 246, 52, 251, 16, 194, 72, 150, 249, 23, 90, 107, 151, 42, 154, 124}; //, 48, 58, 30, 24, 42, 33, 38, 10, 115, 41, 164, 16, 33, 32, 252, 143, 86, 175, 8, 132, 103, 231, 95, 190, 61, 29, 215, 75, 251, 248, 72, 48, 224, 200, 147, 93, 112, 25, 227, 223, 206, 137, 51, 88, 109, 214, 17, 172};
  unsigned char z[NN];
   unsigned char x0[NN] = {0};
   unsigned char inv_x[NN] = {0};
   unsigned char x1[NN] = {0};
   unsigned char tmp[NN]={0};
   unsigned short u=0;
+  time_t t;
+
   rp2(x0);
   rp2(x1);
+
+  n.u[0]=time(&t);
+  n.u[1]=clock();
+for(i=0;i<NN;i++)
+key[i]=n.d[i];
 
   for (i = 0; i < NN; i++)
   inv_x[x0[i]] = i;
