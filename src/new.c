c72559dd6b8831860144c5e479fdc4ea5e1da148
Osamu Sakai <fumumue@protonmail.com>
2021-05-06 23:01:35 +0900

    bugfix

--- src/new.c
+++ src/new.c
@@ -10,15 +10,16 @@ int main(){
     int count=0,i;
 
     while(1){
+        //i=rand();
         aa=chash();
         for(i=0;i<NN/16;i++){
         print_uint128(aa.z[i]);
         printf(" ");
-    }
+        }
         printf("\n");
         //printf("%llu %llu\n",aa.u[0],aa.u[1]);
         count++;
-        if(count==10000)
+        if(count==10000000)
         break;
     }
 
