b5b2f986ef3fa32442f8a03ce295399d39020f9c
Osamu Sakai <fumumue@protonmail.com>
2021-05-06 14:39:34 +0900

    bugfix

--- src/new.c
+++ src/new.c
@@ -7,10 +7,14 @@
 
 void main(){
     arrayul aa={0};
+    int count=0;
 
     while(1){
         aa=chash();
-        printf("%d\n",aa.t[0]);
+        printf("%llu\n",aa.z);
+        count++;
+        if(count==10000)
+        break;
     }
 
 }
